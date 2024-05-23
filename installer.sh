2b9fe7171cd4c3281cca7df65c997ca8273f3dea
Lookey-dev <90994101+Lookey-dev@users.noreply.github.com>
2024-05-22 23:21:31 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 cd ~/skunk-recon
-wget https://gist.githubusercontent.com/six2dez/ffc2b14d283e8f8eff6ac83e20a3c4b4/raw
-mv raw wordlist.txt
+go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+pdtm -install-all
+go install github.com/c3l3si4n/godeclutter@HEAD
+wget https://gist.githubusercontent.com/six2dez/ffc2b14d283e8f8eff6ac83e20a3c4b4/raw -O wordlist.txt
 curl https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt -O
-wget https://github.com/projectdiscovery/nuclei/releases/download/v2.9.7/nuclei_2.9.7_linux_amd64.zip
-unzip nuclei_2.9.7_linux_amd64.zip
-sudo mv nuclei /usr/bin/
-rm nuclei_2.9.7_linux_amd64.zip
-wget https://github.com/projectdiscovery/subfinder/releases/download/v2.6.0/subfinder_2.6.0_linux_amd64.zip
-unzip subfinder_2.6.0_linux_amd64.zip
-sudo mv subfinder /usr/bin/
-rm subfinder_2.6.0_linux_amd64.zip
-wget https://github.com/projectdiscovery/alterx/releases/download/v0.0.2/alterx_0.0.2_linux_amd64.zip
-unzip alterx_0.0.2_linux_amd64.zip
-sudo mv alterx /usr/bin/
-rm alterx_0.0.2_linux_amd64.zip
 wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
 tar -xzf assetfinder-linux-amd64-0.1.1.tgz
 sudo mv assetfinder /usr/bin/
@@ -42,7 +32,3 @@ wget https://github.com/blechschmidt/massdns/archive/refs/tags/v1.0.0.zip
 unzip v1.0.0.zip
 mv massdns-1.0.0 /usr/bin/
 rm v1.0.0.zip
-wget https://github.com/projectdiscovery/httpx/releases/download/v1.3.3/httpx_1.3.3_linux_amd64.zip
-unzip httpx_1.3.3_linux_amd64.zip
-mv httpx /usr/bin/
-rm httpx_1.3.3_linux_amd64.zip
